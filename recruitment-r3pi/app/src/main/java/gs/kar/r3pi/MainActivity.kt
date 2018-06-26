21a8eeee3e6768a5f2dd79947da0eddda0ba2abb
kar <hi@kar.gs>
2018-06-26 15:50:24 +0200

    r3pi: add empty project

---
+++ recruitment-r3pi/app/src/main/java/gs/kar/r3pi/MainActivity.kt
@@ -0,0 +1,12 @@
+package gs.kar.r3pi
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
