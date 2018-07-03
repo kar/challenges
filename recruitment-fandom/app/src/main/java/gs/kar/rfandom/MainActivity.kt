8f5cbba2d4c919860dad194cd8a0f1461d89539a
kar <hi@kar.gs>
2018-07-03 15:39:43 +0200

    fandom: make basic UI

--- recruitment-fandom/app/src/main/java/gs/kar/rfandom/MainActivity.kt
+++ recruitment-fandom/app/src/main/java/gs/kar/rfandom/MainActivity.kt
@@ -1,11 +1,17 @@
 package gs.kar.rfandom
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,26 +19,108 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        viewpager.adapter = TabsPagerAdapter(supportFragmentManager)
+        tablayout.setupWithViewPager(viewpager)
+    }
+
+    inner class TabsPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+        override fun getItem(position: Int): Fragment {
+            return when (position) {
+                0 -> WikisFragment()
+                else -> ImagesFragment()
+            }
+        }
+
+        override fun getPageTitle(position: Int): CharSequence? {
+            return when(position) {
+                0 -> "Titles"
+                else -> "Images"
+            }
+        }
+
+        override fun getCount(): Int {
+            return 2
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    class ImagesFragment : Fragment() {
+
+//        private val update: Update<Message, AbihomeState> by DI.instance()
+//        private val state: State<AbihomeState> by DI.instance()
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            val view = inflater.inflate(R.layout.fragment_images, container, false) as RecyclerView
+            view.layoutManager = GridLayoutManager(context, 2)
+            return view
+        }
+
+        override fun onStart() {
+            super.onStart()
+            bind(view as RecyclerView)
+        }
+
+        private fun bind(view: RecyclerView) {
+//            val imageType = Type<ItemImageBinding>(R.layout.item_image)
+//                    .onBind {
+//                        val url = it.binding.item?.url
+//                        if (url != null) Glide.with(view).load(url).into(it.itemView as ImageView)
+//                    }
+//                    .onClick {
+//                        val image = it.binding.item
+//                        if (image != null) update.send(OnSelectionChange(image))
+//                    }
+//
+//            launch {
+//                val channel = state.subscribe()
+//                val abi = channel.receive()
+//                launch(UI) {
+//                    LastAdapter(abi.images, BR.item).map<Image>(imageType).into(view)
+//                }
+//                channel.cancel()
+//            }
+        }
+
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    class WikisFragment : Fragment() {
 
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+//        private val update: Update<Message, AbihomeState> by DI.instance()
+//        private val state: State<AbihomeState> by DI.instance()
+
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                  savedInstanceState: Bundle?): View? {
+            val view = inflater.inflate(R.layout.fragment_wikis, container, false) as RecyclerView
+            view.layoutManager = GridLayoutManager(context, 2)
+            return view
         }
+
+        override fun onStart() {
+            super.onStart()
+            bind(view as RecyclerView)
+        }
+
+        private fun bind(view: RecyclerView) {
+//            val imageType = Type<ItemImageBinding>(R.layout.item_image)
+//                    .onBind {
+//                        val url = it.binding.item?.url
+//                        if (url != null) Glide.with(view).load(url).into(it.itemView as ImageView)
+//                    }
+//                    .onClick {
+//                        val image = it.binding.item
+//                        if (image != null) update.send(OnSelectionChange(image))
+//                    }
+//
+//            launch {
+//                val channel = state.subscribe()
+//                val abi = channel.receive()
+//                launch(UI) {
+//                    LastAdapter(abi.images, BR.item).map<Image>(imageType).into(view)
+//                }
+//                channel.cancel()
+//            }
+        }
+
     }
 }
